20c7699e3a65edd7038dd931e7da417815676ab1
cjessica34 <74015865+cjessica34@users.noreply.github.com>
2021-07-02 11:53:06 +0200

    Update TestGreeter.java
    someone2

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -20,8 +20,9 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
+    String someone2 = "une string trop longue";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone), containsString(someone2));
   }
 
   @Test
